6b1754904534cfb1de6fbe7aaa1ffbefac05d1d4
James Keenan <jameskeenan295@gmail.com>
2020-01-04 00:13:50 +0000

    Added support for VBNT-Z
    Tested successfully now on VANT-9 & VBNT-Z

--- libautoflashgui.py
+++ libautoflashgui.py
@@ -8,10 +8,9 @@
 
 import mysrp as srp
 from urllib.parse import urlencode
-import binascii, json, urllib, socket, time, sys
+import binascii, json, time, sys, traceback, datetime
 from robobrowser import RoboBrowser
 import liblang
-from bs4 import BeautifulSoup
 
 # init_language():
 # Call this function with sys.argv and sys.path as the paramaters.
@@ -24,14 +23,16 @@ def init_language(argv, path, language):
 def srp6authenticate(br, host, username, password):
     try:
         debugData = []
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' GETing http://' + host)
         br.open('http://' + host)
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Got http://' + host)
         token = br.find(lambda tag: tag.has_attr('name') and tag['name'] == 'CSRFtoken')['content']
         debugData.append('Got CSRF token: ' + token)
-
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Got initial (unauthenticated) CSRFtoken: ' + token)
         usr = srp.User(username, password, hash_alg = srp.SHA256, ng_type = srp.NG_2048)
         uname, A = usr.start_authentication()
         debugData.append(_("A value ") + str(binascii.hexlify(A)))
-
+        
         br.open('http://' + host + '/authenticate', method='post', data = urlencode({'CSRFtoken' : token, 'I' : uname, 'A' : binascii.hexlify(A)}))
         debugData.append("br.response " + str(br.response))
         j = json.decoder.JSONDecoder().decode(br.parsed.decode())
@@ -50,17 +51,17 @@ def srp6authenticate(br, host, username, password):
         usr.verify_session(binascii.unhexlify(j['M']))
         if not usr.authenticated():
             raise Exception(_("Unable to authenticate"))
-
+        
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Authentication successful' + token)
         return True
 
     except Exception:
-        print(_("Authentication failed, debug values are: ") + str(debugData))
-        print(_("Exception: ") + str(sys.exc_info()[0]))
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Authentication failed, debug values are: ") + str(debugData))
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Exception: ") + str(sys.exc_info()[0]))
         traceback.print_exc()
         raise
         
 def runCommand(br, host, token, activeMethod, activeCommand, ddnsService):
-    print("Sending command: " + activeCommand)
     if activeMethod == 'Ping':
         postdata = {'CSRFtoken': token, 'action': 'PING', 'ipAddress': ':::::::;' + activeCommand, 'NumberOfRepetitions': '3', 'DataBlockSize': '64'}
         urlpostfix = '/modals/diagnostics-ping-modal.lp'
@@ -81,11 +82,12 @@ def runCommand(br, host, token, activeMethod, activeCommand, ddnsService):
             'action': 'SAVE',
             'CSRFtoken': token
         }
-    elif activeMethod == 'VodafoneDDNS':
+        urlpostfix = '/dyndns.lp'
+    elif activeMethod == 'VodafoneDDNS': # DGA0130 Vodafone NZ VANT-9 Ultra Hub
         postdata = {
             'ddnsStatus': '1',
             'ddnsService': ddnsService,
-            'ddnsDomain': 'testzz2.com;' + activeCommand,
+            'ddnsDomain': 'test.com;' + activeCommand,
             'ddnsUsrname': 'admin',
             'ddnsPswrd': 'notarealpassword',
             'securedns': '0',
@@ -93,59 +95,61 @@ def runCommand(br, host, token, activeMethod, activeCommand, ddnsService):
             'CSRFtoken': token
             }
         urlpostfix = '/modals/dns-ddns.lp'
-    elif activeMethod == 'VodafoneDDNS_http':
-        import http.server, socketserver, threading
-        Handler = http.server.SimpleHTTPRequestHandler
-        port = 8000
-        def start_server(path, port=80):
-            httpd = socketserver.TCPServer(("", port), Handler)
-            httpd.serve_forever()
-        # Start the http server in a new thread so we can continue executing our commands
-        daemon = threading.Thread(name='daemon_server',
-                                  target=start_server,
-                                  args=('.', port))
-        daemon.setDaemon(True) # Set as a daemon so it will be killed once the main thread is dead.
-        daemon.start()
+    elif activeMethod == 'VodafoneDDNS2': # DNA0130 Vodafone NZ VBNT-Z Ultrahub Plus
         postdata = {
             'ddnsStatus': '1',
             'ddnsService': ddnsService,
-            'ddnsDomain': 'testzz2.com;' + activeCommand,
+            'ddnsDomain': 'test.com;' + activeCommand,
             'ddnsUsrname': 'admin',
             'ddnsPswrd': 'notarealpassword',
             'securedns': '0',
             'action': 'SAVE',
             'CSRFtoken': token
             }
-        urlpostfix = '/modals/dns-ddns.lp'
+        urlpostfix = '/modals/internet/dns_ddns.lp'
     else:
         raise Exception(_("Unknown method ") + activeMethod + _(" please check input in GUI"))
     
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + " Sending command: " + activeCommand)
     r = br.session.post('http://' + host + urlpostfix, data=postdata)
-    print(r)
-    print(r.content)
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Response: ' + str(br.response))
     br._update_state(r)
     
     return br.response.ok
 
 
 def mainScript(host, username, password, flashFirmware, upgradeFilename, flashSleepDelay, activeMethod, activeCommand, splitCommand, ddnsService, connectRetryDelay, interCommandDelay):
-    br = RoboBrowser(history=True, parser="html.parser")
+    br = RoboBrowser(history=True, parser="html.parser", timeout=15)
 
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Authenticating"))
+    srp6authenticate(br, host, username, password)
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' GETing : http://' + host + ' to aquire authenticated CSRFtoken')
+    br.open('http://' + host)
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' GET completed: ' + str(br.response))
+    if activeMethod == 'VodafoneDDNS' or activeMethod == 'VodafoneDDNS2':
+        token = br.find(lambda tag: tag.has_attr('name') and tag.has_attr('type') and tag['name'] == 'CSRFtoken')['value']
+    else:
+        token = br.find(lambda tag: tag.has_attr('name') and tag['name'] == 'CSRFtoken')['content']
+    print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Got authenticated CSRFtoken: ' + token)
     success = False
     if flashFirmware:
-        print(_("Authenticating"))
-        srp6authenticate(br, host, username, password)
-        br.open('http://' + host)
-        token = br.find(lambda tag: tag.has_attr('name') and tag['name'] == 'CSRFtoken')['content']
-        print(_("Sending flash command to modem"))
-        filedata = {'CSRFtoken': token, 'upgradefile': (upgradeFilename, open(upgradeFilename, 'rb'))}
-        r = br.session.post('http://' + host + '/modals/gateway-modal.lp?action=upgradefw', files=filedata)
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Flash Firmware option is enabled. Activemethod = ' + activeMethod)
+        if activeMethod == 'VodafoneDDNS': # DGA0130 Vodafone NZ VANT-9 Ultra Hub
+            upgradeurlpostfix = '/modals/upgrade.lp?action=upgradefw'
+        elif activeMethod == 'VodafoneDDNS2': # DNA0130 Vodafone NZ VBNT-Z Ultrahub Plus
+            upgradeurlpostfix = '/modals/settings/firmwareUpdate.lp?action=upgradefw'
+        else:
+            upgradeurlpostfix = '/modals/gateway-modal.lp?action=upgradefw'
+        filedata = {'CSRFtoken': token, 'upgradefile': ('test.rbi', open(upgradeFilename, 'rb'))}
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' POSTing firmware to: ' + 'http://' + host + upgradeurlpostfix)
+        r = br.session.post('http://' + host + upgradeurlpostfix, files=filedata)
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Fimrware POST completed: ' + str(br.response))
         br._update_state(r)
         print(r.text)
         if r.text == '{ "success":"true" }':
-            print(_("Modem reports flashing commenced successfully"))
+            print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Modem reports flashing commenced successfully"))
             success = True
-            print(_("Waiting for reboot... Sleeping for %s s") % (flashSleepDelay))
+            print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Waiting for reboot... Sleeping for %s s") % (flashSleepDelay))
             time.sleep(int(flashSleepDelay))
     else:
         success = True
@@ -155,38 +159,28 @@ def mainScript(host, username, password, flashFirmware, upgradeFilename, flashSl
         attempt = 0
         while not backUp:
             attempt += 1
-            print(_("Connect attempt %i") % (attempt))
+            print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Connect attempt %i") % (attempt))
             try:
                 br.open('http://' + host)
-                print (br.response)
+                print ('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' Response: ' + str(br.response))
                 if br.response.ok:
                     backUp = True
             except Exception:
-                print(_('Failed to connect, attempt %i.  Retrying') % (attempt))
+                print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _('Failed to connect, attempt %i.  Retrying') % (attempt))
                 time.sleep(int(connectRetryDelay))
                 pass
 
-        print(_("Modem up"))
-
-    print(_("Authenticating"))
-    srp6authenticate(br, host, username, password)
-    print('GETing http://' + host + ' to aquire CSRFtoken')
-    response = br.session.get('http://' + host)
-    print(br.response)
-    print('got it')
-    # note: there was a bug with br.response.content (and br.find) that prevented finding the CSRF token directly from br.find, so we have to use response, and beautifulsoup. Same thing, just a few more lines of code.
-    soup = BeautifulSoup(response.content, 'html.parser')
-    token = soup.find(lambda tag: tag.has_attr('name') and tag['name'] == 'CSRFtoken')['value'] # VANT-9 & VBNT-Z CSRFtoken is embeded in javascript and uses 'value' instead of 'content'
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Modem up"))
 
     if not splitCommand:
         runCommand(br, host, token, activeMethod, activeCommand, ddnsService)
     else:
-        print(_("Splitting command up using semicolons"))
+        print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Splitting command up using semicolons"))
         for subCommand in [s for s in activeCommand.split(';') if len(s) > 0]:
             runCommand(br, host, token, activeMethod, subCommand, ddnsService)
-            print(_("Sleeping..."))
+            print('{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Sleeping...") + str(int(interCommandDelay)) + ' seconds')
             time.sleep(int(interCommandDelay))
 
-    result = _("Please try a ssh connection now to ") + host + _(" with username root and password root (change password immediately with passwd!)  Rebooting your modem now is recommended to stop any services that have been disabled.")
+    result = '{0:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()) + ' ' + _("Please try a ssh connection now to ") + host + _(" with username root and password root (change password immediately with passwd!)  Rebooting your modem now is recommended to stop any services that have been disabled.")
     print(result)
     return result
